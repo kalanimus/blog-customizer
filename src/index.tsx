1fa2e667be53b7886cb2b570bf57f3bcfafd259b
Nikita Kalanchekaev <nikita.kalan@yandex.ru>
2025-06-13 15:53:39 +0300

    begining

--- src/index.tsx
+++ src/index.tsx
@@ -5,14 +5,19 @@ import clsx from 'clsx';
 import { Article } from './components/article/Article';
 import { ArticleParamsForm } from './components/article-params-form/ArticleParamsForm';
 import { defaultArticleState } from './constants/articleProps';
+// import { defaultArticleState, ArticleStateType} from './constants/articleProps';
 
 import './styles/index.scss';
 import styles from './styles/index.module.scss';
+
+// import { useState } from 'react';
 
 const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
 
 const App = () => {
+	// const [appState, setAppState] = useState<ArticleStateType>(defaultArticleState);
+
 	return (
 		<main
 			className={clsx(styles.main)}
